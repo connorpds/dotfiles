84e5229cf21e964e462765d4bd04959e46c080b2
komodo <connorselna2021@u.northwestern.edu>
2025-09-24 23:46:43 -0400

    setup script prettier much done

--- bash-tmux-nvim_setup.sh
+++ bash-tmux-nvim_setup.sh
@@ -1,3 +1,7 @@
+#copies the contents of dotfiles/config to ~/.config, adds a 
+#source line to the bashrc. bashrc and config together activity
+#together handle tmux configuration.
+
 echo "Preparing to copy ${HOME}/dotfiles/config to ${HOME}/.config."
 bash "${HOME}/dotfiles/setup/copy_configs.sh"
 
