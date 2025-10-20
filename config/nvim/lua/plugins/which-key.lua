88facefda8b786bc0c23fb1bff41c81d80785586
komodo <connorselna2021@u.northwestern.edu>
2025-10-19 22:00:26 -0400

    well, i sure refined which-key nicely only to decide i dont wan't it

--- config/nvim/lua/plugins/which-key.lua
+++ config/nvim/lua/plugins/which-key.lua
@@ -6,15 +6,33 @@ return {
 
     local wk = require("which-key")
     wk.setup({
+      opts = {
+          triggers = {
+            {"<leader>", mode = {"n", "v",},},
+            {"g", mode = {"n", "v",},},
+        },
+      },
       icons = {
         breadcrumb = ">>",   -- top line path
         separator  = "->",   -- group separator
-
         group      = "+",    -- group marker
         mappings   = false,  -- NO per-mapping icons (prevents nerd/emoji)
+        ellipsis = "...",
+        --set all of the key "icons" manually since we are avoiding nerdfonts
         keys = {
           Esc = "esc",
           BS = "bksp",
+          Up = "↑",
+          Down = "↓",
+          Right = "→",
+          Left = "←",
+          C = "ctrl",
+          M = "alt", --the M is for "Meta"
+          D = "⌘",
+          S = "shift",
+          CR = "enter",
+          ScrollWheelDown = "SW↓",
+          ScrollWheelUp = "SW↑",
         },
       },
       win = {
